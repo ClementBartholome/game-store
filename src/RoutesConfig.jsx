a9c44728721645efa0b30ad7b4844eec44de7fa6
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-06-28 19:30:40 +0200

    props GamePage

--- src/RoutesConfig.jsx
+++ src/RoutesConfig.jsx
@@ -3,9 +3,15 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import GamePage from "./pages/GamePage";
 import PageNotFound from "./pages/404";
-import CartPage from "./components/CartPage";
+import CartPage from "./pages/CartPage";
 
-const RoutesConfig = ({ games, addToCart, removeFromCart, cartItems }) => {
+const RoutesConfig = ({
+  games,
+  addToCart,
+  removeFromCart,
+  cartItems,
+  isInCart,
+}) => {
   return (
     <Routes>
       <Route
@@ -26,7 +32,18 @@ const RoutesConfig = ({ games, addToCart, removeFromCart, cartItems }) => {
           <CartPage cartItems={cartItems} removeFromCart={removeFromCart} />
         }
       />
-      <Route path="/game/:name" element={<GamePage games={games} />} />
+      <Route
+        path="/game/:name"
+        element={
+          <GamePage
+            games={games}
+            isInCart={isInCart}
+            handleCartClick={addToCart}
+            cartItems={cartItems}
+            removeFromCart={removeFromCart}
+          />
+        }
+      />
       <Route path="/*" element={<PageNotFound />} />
       <Route path="/404" element={<PageNotFound />} />
     </Routes>
