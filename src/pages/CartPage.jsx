9163a8d4d47461e90e3cfc9b248edb1853316619
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-06-28 19:31:10 +0200

    Déplacement CartPage.jsx dans dossier pages

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CartItem from "./CartItem";
+import CartItem from "../components/CartItem";
 
 const CartPage = ({ cartItems, removeFromCart }) => {
   const total = cartItems.reduce((acc, item) => acc + item.price, 0);
