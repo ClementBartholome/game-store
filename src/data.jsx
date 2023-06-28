02f993ba46305b6015ad6deaf80e23ce87e0a6b3
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-06-28 19:30:17 +0200

    ajout des specs et images pour chaque jeu

--- src/data.jsx
+++ src/data.jsx
@@ -6,8 +6,18 @@ const games = [
       "Explorez un monde ouvert plein de vie dans les paysages du Mexique et vibrez au volant de voitures d'exception. Partez à la conquête des pistes accidentées de la Sierra Nueva dans cette expérience ultime du Rallye Horizon.",
     price: 59.99,
     tags: ["Racing", "Sports"],
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1551360/header.jpg?t=1683059745",
     images: [
       "https://cdn.cloudflare.steamstatic.com/steam/apps/1551360/ss_00f0090174380eeaf8753bd3d1028b6772c3aebf.1920x1080.jpg?t=1683059745",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1551360/ss_cf56e25a0290556ba83229eb0ab370d10be0407c.1920x1080.jpg?t=1683059745",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1551360/ss_b65236b365315ebb6da6114ce42cd74b59cab3c8.1920x1080.jpg?t=1683059745",
     ],
   },
   {
@@ -17,8 +27,18 @@ const games = [
       "Un monde immense aux environnements riches et variés, parsemé d'obscurs et tortueux donjons tous reliés naturellement entre eux, vous attend. Au fil de votre exploration, goûtez à l'inconnu, bravez les menaces permanentes et accomplissez votre destinée.",
     price: 59.99,
     tags: ["Action-RPG"],
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/header.jpg?t=1683618443",
     images: [
       "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/ss_ae44317e3bd07b7690b4d62cc5d0d1df30367a91.1920x1080.jpg?t=1683618443",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/ss_3e556415d1bda00d749b2166ced264bec76f06ee.1920x1080.jpg?t=1683618443",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1245620/ss_1e3dfe515c04f4071207f01d62b85a1d6b560ced.1920x1080.jpg?t=1683618443",
     ],
   },
   {
@@ -28,8 +48,18 @@ const games = [
       "Incarnez Nathan Drake et Chloe Frazer dans leurs aventures indépendantes, où chacun devra confronter son passé et forger son héritage. Ce jeu contient les aventures solo acclamées par la critique UNCHARTED 4: A Thief's End et UNCHARTED: The Lost Legacy.",
     price: 49.99,
     tags: ["Aventure"],
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1659420/header_french.jpg?t=1672777947",
     images: [
-      "https://cdn.cloudflare.steamstatic.com/steam/apps/1659420/ss_a2f6322488a915ad00af9a724936d7ca3868f1fd.1920x1080.jpg?t=1672777947",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1659420/ss_a1816dd536122dd740043c2b9440136503904215.1920x1080.jpg?t=1672777947",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1659420/ss_e570e5f369ff6cd0471f80d99f5a86c3b03a2285.1920x1080.jpg?t=1672777947",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1659420/ss_b171bf276c5f6edb99ca96be0912b64b9136c2ad.1920x1080.jpg?t=1672777947",
     ],
   },
   {
@@ -39,8 +69,18 @@ const games = [
       "Dépassez le point de rupture dans EA SPORTS™ F1® 23, le jeu vidéo officiel du 2023 FIA Formula One World Championship™. Retrouvez un nouveau chapitre du mode histoire « Point de rupture » palpitant, avec ses drames à grande vitesse et ses rivalités enflammées.",
     price: 69.99,
     tags: ["Racing", "Sports"],
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/2108330/header.jpg?t=1687440034",
     images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/2108330/ss_d79565a1996470eb4a9440f62022809aeac4e519.1920x1080.jpg?t=1687440034",
       "https://cdn.cloudflare.steamstatic.com/steam/apps/2108330/ss_011491f13ab703b6e5827f9ceca7fafe8e18f4bb.1920x1080.jpg?t=1687440034",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/2108330/ss_7508416720de6e058f258f645418f9d82ecb213a.1920x1080.jpg?t=1687440034",
     ],
   },
   {
@@ -50,8 +90,18 @@ const games = [
       "Rejoignez plus de 20 millions de joueurs dans le RPG multijoueur en ligne primé, pour vivre une aventure sans limite dans un univers Elder Scrolls persistant. Combat, artisanat, sièges, exploration... créez votre propre style de jeu, sans abonnement au jeu nécessaire.",
     price: 19.99,
     tags: ["MMO", "RPG"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/306130/header.jpg?t=1685974187",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/306130/ss_6ce59dcaa51a89208c643301f6e220d75c001b97.1920x1080.jpg?t=1685974187",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/306130/ss_c1cdae9879550709486774eed3a2760d18955349.1920x1080.jpg?t=1685974187",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/306130/ss_16688139333e39593af0eccc77342165eacae0d0.1920x1080.jpg?t=1685974187",
     ],
   },
   {
@@ -61,8 +111,18 @@ const games = [
       "Voici le tout nouveau challenger de Capcom ! Street Fighter™ 6, le dernier opus de la série, sera disponible mondialement le 2 juin 2023 ! L'expérience Street Fighter 6 s'étend sur trois modes de jeu distincts : World Tour, Fighting Ground et Battle Hub.",
     price: 59.99,
     tags: ["Versus Fighting"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/1364780/header.jpg?t=1686291121",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1364780/ss_387137f8cccb048c35a8685634372e97785d40aa.1920x1080.jpg?t=1686291121",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1364780/ss_a381f1b3b450c18900d47b991ce8e7456e9cdba5.1920x1080.jpg?t=1686291121",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1364780/ss_f62ce93269a6d8e0027853358af4d6368e2c4b93.1920x1080.jpg?t=1686291121",
     ],
   },
   {
@@ -72,8 +132,18 @@ const games = [
       "Call of Duty®: Modern Warfare® II plonge les joueurs dans un conflit mondial sans précédent, avec le retour des Opérateurs emblématiques de la Task Force 141.",
     price: 59.99,
     tags: ["Shooter"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/1938090/header.jpg?t=1687571108",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1938090/ss_69daadf0ed608ae40e57c520d60e6d38e14dffe9.1920x1080.jpg?t=1687571108",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1938090/ss_892f9c8a7be3d758f39ecb90bccd1129c912f3cb.1920x1080.jpg?t=1687571108",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1938090/ss_da11f26c8fec6def6e235786e11b868eb8b115ef.1920x1080.jpg?t=1687571108",
     ],
   },
   {
@@ -83,8 +153,18 @@ const games = [
       "Starfield est le premier nouvel univers en 25 ans de Bethesda Game Studios, les créateurs récompensés de The Elder Scrolls V: Skyrim et Fallout 4.",
     price: 69.99,
     tags: ["RPG"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/1716740/header.jpg?t=1687290127",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1716740/ss_4887dc140a637684ddcfca518458668409f946dc.1920x1080.jpg?t=1687290127",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1716740/ss_b2821283cb140cd5a6289a8160016b6a60d8f96e.1920x1080.jpg?t=1687290127",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1716740/ss_68f15d580bf91971f637be5e464bc803482d78f7.1920x1080.jpg?t=1687290127",
     ],
   },
   {
@@ -94,8 +174,18 @@ const games = [
       "Anno 1800™ – Prenez la tête de la Révolution industrielle ! Bienvenue à l'aube de l'Ère industrielle. La voie que vous tracerez changera la face de votre monde. Serez-vous un innovateur ou un profiteur ? Un conquérant ou un libérateur ? De vous seul dépend la trace que vous laisserez dans l'histoire.",
     price: 14.99,
     tags: ["Gestion"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/916440/header.jpg?t=1681854014",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/916440/ss_9756553b540fbfefc2d96baafc33aecd7ef1dc44.1920x1080.jpg?t=1681854014",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/916440/ss_cad25bea4177e0c49c104c54138c0b8ba4c1ab3e.1920x1080.jpg?t=1681854014",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/916440/ss_5cd9e958b856c98900a65328b51bfe41636e0bde.1920x1080.jpg?t=1681854014",
     ],
   },
   {
@@ -105,8 +195,18 @@ const games = [
       "Montez votre équipe de rêve, surpassez la concurrence et vivez les grands frissons des soirées européennes dans l'UEFA Champions League. Votre épopée vers la gloire footballistique vous attend.",
     price: 59.99,
     tags: ["Sports"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/1904540/header.jpg?t=1680627762",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1904540/ss_278762c69a8851cc77f7ae425958444f05e155d9.1920x1080.jpg?t=1680627762",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1904540/ss_2df9bd53ae1a332c15b3a0a26a44e8d492170a1e.1920x1080.jpg?t=1680627762",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/1904540/ss_e9f96e18061a50212c39e1561168bb5b607ef115.1920x1080.jpg?t=1680627762",
     ],
   },
   {
@@ -116,8 +216,18 @@ const games = [
       "Échappez à une planète étrangère chaotique en affrontant des hordes de monstres déchaînés, en solo ou entre amis. Combinez le butin de façon surprenante et maîtrisez chacun des personnages jusqu'à devenir vous-même le chaos que vous craigniez tant lors de votre premier écrasement.",
     price: 24.99,
     tags: ["Shooter"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/632360/header.jpg?t=1660063598",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/632360/ss_2bb49071317f7b241a527cf6e7aabd2cb6af055b.1920x1080.jpg?t=1660063598",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/632360/ss_a3f57f281813cb51cb5d919701470acb962ff297.1920x1080.jpg?t=1660063598",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/632360/ss_328d6fcb223f848c2a1047bb86702c4175d92317.1920x1080.jpg?t=1660063598",
     ],
   },
   {
@@ -127,8 +237,18 @@ const games = [
       "Sifu est un jeu de combat réaliste à la troisième personne, avec des mécaniques pointues de kung-fu et une action digne des films d'arts martiaux qui vous entraîne dans une quête de vengeance.",
     price: 39.99,
     tags: ["Action"],
-    images: [
+    specs: {
+      link: "https://www.xbox.com/fr-FR/games/forza-horizon-5",
+      release: "1er novembre 2021",
+      developer: "Playground Games",
+      publisher: "Xbox Game Studios",
+    },
+    cover:
       "https://cdn.cloudflare.steamstatic.com/steam/apps/2138710/header.jpg?t=1683797995",
+    images: [
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/2138710/ss_fcbdfe30cb361fdc6971878306a5179d175240c9.1920x1080.jpg?t=1683797995",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/2138710/ss_c917c981f912e8331b40a2638a32b3021458d4b8.1920x1080.jpg?t=1683797995",
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/2138710/ss_6541bc550e7b1133d123ebce8b0e098dccbe66eb.1920x1080.jpg?t=1683797995",
     ],
   },
   // {
