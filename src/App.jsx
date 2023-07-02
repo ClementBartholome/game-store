7c5aeddfd6fc600e14d3616fc3c4d563044182b1
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-07-02 22:25:49 +0200

    feat: sauvegarde du panier dans le localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import RoutesConfig from "./RoutesConfig";
 import games from "./data";
 
 const App = () => {
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    JSON.parse(localStorage.getItem("cartItems")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   const removeFromCart = (gameId) => {
     setCartItems(cartItems.filter((item) => item.id !== gameId));
