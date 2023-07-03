968e67ffddb9485b1386b71f8cc3e62c92ee6c92
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-07-03 18:06:12 +0200

    feat: ajout de l'authentification

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "../images/hero.svg";
 import Carrousel from "../components/Carrousel";
 import { Link } from "react-router-dom";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 
 export default function HomePage({ games }) {
   const gameCovers = games.slice(0, 6).map((game) => game.cover);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userLoggedIn = localStorage.getItem("isLoggedIn") === "true";
+    setIsLoggedIn(userLoggedIn);
+  }, []);
+
+  const handleLogin = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result.user);
+        setIsLoggedIn(true);
+        localStorage.setItem("isLoggedIn", "true");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const handleLogout = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        setIsLoggedIn(false);
+        localStorage.setItem("isLoggedIn", "false");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <main className="homepage">
@@ -13,8 +49,18 @@ export default function HomePage({ games }) {
           <p>Découvrez vos prochains jeux préférés</p>
           <h1>Game Hub</h1>
           <div className="register-buttons">
-            <button className="login">Connexion</button>
-            <button className="register">Inscription</button>
+            {isLoggedIn ? (
+              <button className="logout" onClick={handleLogout}>
+                Déconnexion
+              </button>
+            ) : (
+              <>
+                <button className="login" onClick={handleLogin}>
+                  Connexion
+                </button>
+                <button className="register">Inscription</button>
+              </>
+            )}
           </div>
         </div>
         <div className="right-container">
