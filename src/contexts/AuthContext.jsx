58b019be49e23db939234a07a5a3d04bb4bb77b6
ClementBartholome <123072377+ClementBartholome@users.noreply.github.com>
2023-07-09 21:08:27 +0200

    suppression ancien code

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -10,39 +10,49 @@ import {
 
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-
 const AuthContext = createContext();
 
 export default AuthContext;
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(localStorage.getItem("user") || null);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
+    // Subscribe to the authentication state changes
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
+        // If user is authenticated, update the user state and log the user object
         setUser(user);
         console.log(user);
       } else {
+        // If user is not authenticated, set the user state to null
         setUser(null);
       }
     });
 
+    // Unsubscribe from the authentication state changes when the component is unmounted
     return () => {
       unsubscribe();
     };
   }, []);
 
+  // Handle the login action
   const handleLogin = async () => {
+    // Sign in with the Google popup provider
     const { user } = await signInWithPopup(auth, provider);
+    // Update the user state
     setUser(user);
   };
 
+  // Handle the logout action
   const handleLogout = async () => {
+    // Sign out the user
     await signOut(auth);
+    // Set the user state to null
     setUser(null);
   };
 
+  // Create the authentication context value
   const authContextValue = {
     user,
     handleLogin,
@@ -50,6 +60,7 @@ export function AuthProvider({ children }) {
   };
 
   return (
+    // Provide the authentication context value to child components
     <AuthContext.Provider value={authContextValue}>
       {children}
     </AuthContext.Provider>
