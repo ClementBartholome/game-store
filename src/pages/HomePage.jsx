72a0419bcefb111aa7d14a2cff50217758076bb4
Clément Bartholomé <clementoss@gmail.com>
2023-09-18 21:57:07 +0200

    remove signup button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -24,9 +24,9 @@ export default function HomePage() {
             ) : (
               <>
                 <button className="login" onClick={handleLogin}>
-                  Connexion
+                  Connexion avec Google
                 </button>
-                <button className="register">Inscription</button>
+                {/* <button className="register">Inscription</button> */}
               </>
             )}
           </div>
